92047d8fc3bcee48d8a184f14e80293a66a56bb2
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-09 14:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form1');
+    
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const select = document.getElementById('colorSelect');
+        const selectedIndex = select.selectedIndex;
+        if (selectedIndex === -1) {
+            return;
+        }
+        select.remove(selectedIndex);   
+    });
+});
